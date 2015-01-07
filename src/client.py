d963bec1671e4eb9c25b9f4ebcfb0d28c6aa4b0a
De Jonckheere Stéphane <humble.jok@gmail.com>
2015-01-07 22:08:12 +0100

    Working

--- src/client.py
+++ src/client.py
@@ -4,12 +4,15 @@ import threading
 
 
 def print_message(channel, method, properties, body):
+    print 'RECEIVED:' + body
     data = body.split('@')
     print data[0] + ' says:' + data[1] 
 
 def listening(user_name, channel):
-    channel.queue_bind(exchange='imessage', queue='all_messages', routing_key=user_name)
-    channel.basic_consume(print_message, queue='all_messages', no_ack=True)
+    print "Registering to " + user_name
+    result = channel.queue_declare(exclusive=True)
+    channel.queue_bind(exchange='imessage', queue=result.method.queue, routing_key=user_name)
+    channel.basic_consume(print_message, queue=result.method.queue, no_ack=True)
     channel.start_consuming()
 
 def main():
@@ -23,20 +26,15 @@ def main():
         print "Exchange already exists or some error!"
         traceback.print_exc()
         print "**************************************"
-    try:
-        channel.queue_declare(queue='all_messages', durable=True)
-    except:
-        print "Exchange already exists or some error!"
-        traceback.print_exc()
-        print "**************************************"
     listener = threading.Thread(target=listening, args = (user_name, channel))
     listener.start()
     data = raw_input(">")
     while data!='quit':
         if '@' in data:
             target = data.split('@')[0]
-            print 'sending to ' + target 
-            channel.basic_publish(exchange='imessage', routing_key=target, body=data)
+            message = data.split('@')[1]
+            print 'sending to ' + target + ' -> [' + message + ']'
+            channel.basic_publish(exchange='imessage', routing_key=target, body=user_name + '@' + message)
         data = raw_input(">")
     listener._Thread__stop()
 if __name__ == "__main__":
