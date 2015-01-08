404e4e67397d66c76f5ca883eaa2eb68c72f501b
De Jonckheere Stéphane <humble.jok@gmail.com>
2015-01-08 23:05:50 +0100

    RPC Admin Server

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,28 @@
-def main():
-    user_name = raw_input('User name:')
-    print "Hello " + user_name
+import pika
+from admin_message_pb2 import AdminCommand
 
+def handle_message(channel, method, properties, body):
+    command = AdminCommand()
+    command.ParseFromString(body)
+    print properties.reply_to
+    
+    channel.basic_publish(exchange='admin_exchange',
+                          routing_key=properties.reply_to,
+                          properties=pika.BasicProperties(correlation_id = properties.correlation_id),
+                          body='Une reponse')
+    channel.basic_ack(delivery_tag = method.delivery_tag)
+
+def main():
+    print "Starting server"
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.9.93'))
+    channel = connection.channel()
+    channel.queue_declare(queue='admin_queue')
+    
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(handle_message, queue='admin_queue')
+    
+    print " [x] Awaiting requests"
+    channel.start_consuming()
+    
 if __name__ == "__main__":
     main()
--- src/client.py
+++ src/client.py
@@ -1,15 +1,32 @@
 import pika
 import traceback
 import threading
+from admin_message_pb2 import AdminCommand, AdminControl
+import uuid
 
+callback_queue = None
 
+def handle_admin_request(channel, method, properties, body):
+    print body
+
+def wait_admin_request(channel):
+    channel.basic_consume(handle_admin_request, no_ack=True, queue=callback_queue)
+    
+def send_admin_request(channel, request):
+    corr_id = str(uuid.uuid4())
+    channel.basic_publish(exchange='',
+                    routing_key='admin_queue',
+                    properties=pika.BasicProperties(
+                        reply_to = callback_queue,
+                        correlation_id = corr_id,
+                    ),
+                    body=request.SerializeToString())
 def print_message(channel, method, properties, body):
     print 'RECEIVED:' + body
     data = body.split('@')
     print data[0] + ' says:' + data[1] 
 
 def listening(user_name, channel):
-    print "Registering to " + user_name
     result = channel.queue_declare(exclusive=True)
     channel.queue_bind(exchange='imessage', queue=result.method.queue, routing_key=user_name)
     channel.basic_consume(print_message, queue=result.method.queue, no_ack=True)
@@ -17,9 +34,28 @@ def listening(user_name, channel):
 
 def main():
     user_name = raw_input('User name:')
-    print "Hello " + user_name
+    
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.9.93'))
     channel = connection.channel()
+    
+    admin_control = AdminControl()
+    admin_control.authentication_key = 'This is moi the user -> ' + user_name
+    admin_request = AdminCommand()
+    admin_request.user = user_name
+    admin_request.command = 'login'
+    admin_request.control.MergeFrom(admin_control)
+    result = channel.queue_declare(exclusive=True)
+    callback_queue = result.method.queue
+    admin = threading.Thread(target=wait_admin_request, args = (channel,))
+    admin.start()
+    
+    send_admin_request(channel, admin_request)
+    
+    admin.join()
+    
+    print "Hello " + user_name
+    
+    
     try:
         channel.exchange_declare(exchange='imessage', type='direct')
     except:
@@ -37,5 +73,6 @@ def main():
             channel.basic_publish(exchange='imessage', routing_key=target, body=user_name + '@' + message)
         data = raw_input(">")
     listener._Thread__stop()
+    admin._Thread_stop()
 if __name__ == "__main__":
     main()
